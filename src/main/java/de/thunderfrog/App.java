fdb77adcb21f48bf693cb17702ff37d2e6c3dc25
VanKhaos <christian.richter1987@gmail.com>
2021-08-03 09:00:08 +0200

    init project

---
+++ src/main/java/de/thunderfrog/App.java
@@ -0,0 +1,13 @@
+package de.thunderfrog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
