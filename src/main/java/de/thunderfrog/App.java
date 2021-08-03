83c9f344dec7cfe6779ef418d5efaca8094b4750
VanKhaos <christian.richter1987@gmail.com>
2021-08-03 10:03:31 +0200

    add App.java

--- src/main/java/de/thunderfrog/App.java
+++ src/main/java/de/thunderfrog/App.java
@@ -1,13 +1,21 @@
 package de.thunderfrog;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //  AppInfo
+        MyAppInfo.createAppInfo("String Ding Maven", "Christian Richter", "1.0.2");
+
+        //  CamelCase String True
+        String myString = "string-ding-maven";
+        System.out.println(myString);
+        String camelStringTrue = StringWizard.camelCase(true,myString,'-');
+        System.out.println(camelStringTrue);
+
+        //  CamelCase String False
+        String camelStringFalse = StringWizard.camelCase(false,myString,'-');
+        System.out.println(camelStringFalse);
     }
+
 }
